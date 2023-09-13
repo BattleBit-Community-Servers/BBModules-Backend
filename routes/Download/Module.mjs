32da204c7179069d3188d8d2ac7ed43a416b0edf
MevnG <gueganmeven@gmail.com>
2023-09-13 12:38:14 +0200

    refactor of module download + better handle errors

--- routes/Download/Module.mjs
+++ routes/Download/Module.mjs
@@ -1,6 +1,5 @@
-//GetLastVersionByName.mjs
+// Download/Module.mjs
 
-import fs from 'fs';
 import * as path from 'path';
 import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
@@ -8,8 +7,9 @@ const __dirname = path.dirname(__filename);
 
 import prisma from '../../database/Prisma.mjs';
 
-//const basePath = '/home/apirunner/htdocs/apirunner.mevng.net/MODULES/';
-const basePath = '../../MODULES/'
+const basePath = '../../MODULES/';
+
+let filePath = '';
 
 const metadata = {
   type: 'GET',
@@ -20,108 +20,82 @@ const metadata = {
 
 const func = async (req, res) => {
   try {
-    const moduleName = req.params.moduleName;
-    const version = req.params.version;
 
-    if(version == 'latest'){
-      const lastVersion = await prisma.versions.findMany({
-        where: {
-          modules: {
-            Module_name: moduleName
-          }
-        },
-        orderBy: {
-          Version_created_at: "desc"
-        },
-        select: {
-          Version_v_number: true,
-          Version_file_path: true,
-          Version_approved: true,
-          Version_id: true,
-          modules: {
-            select: {
-              Module_id: true,
-              users: {
-                select: {
-                  User_discord_id: true
-                }
+    let versionsQuery = {
+      where: {
+        modules: {
+          Module_name: req.params.moduleName
+        }
+      },
+      orderBy: {
+        Version_created_at: "desc"
+      },
+      select: {
+        Version_v_number: true,
+        Version_file_path: true,
+        Version_approved: true,
+        Version_id: true,
+        modules: {
+          select: {
+            Module_id: true,
+            users: {
+              select: {
+                User_discord_id: true
               }
             }
           }
         }
-      });
+      }
+    };
 
+    // If want latest version
+    if(req.params.version != 'latest'){
+      const searchCondition = {
+        modules: {
+          Module_name: req.params.moduleName,
+        },
+        Version_v_number: req.params.version,
+      }
 
-      let filePath = '';
+      versionsQuery.where = {
+        ...versionsQuery.where,
+        ...searchCondition
+      };
 
+      const version = await prisma.versions.findFirst(versionsQuery)
+
+      console.log(version)
+
+      if(version && version.Version_approved){
+        filePath = version.Version_file_path;
+      }
+
+    }
+    // If you specify the version manually
+    else{
+
+      const lastVersion = await prisma.versions.findMany(versionsQuery)
+            
       for (let i = 0; i < lastVersion.length; i++) {
         const el  = lastVersion[i];
         if(el.Version_approved){
           filePath = el.Version_file_path;
         }
       }
+    }
 
-      if(filePath !== ''){
-        const fp = path.join(__dirname, basePath, filePath);
-        res.download(fp, (err) => {
-          if (err) {
-            res.status(404).send('File not found');
-          } else {
-            console.log('File download successful');
-          }
-        });
-      }else{
-        res.status(401).json({ message: 'Version not approved' });
-      }
-    }else{
-      const v = await prisma.versions.findFirst({
-        where: {
-          modules: {
-            Module_name: moduleName,
-          },
-          Version_v_number: version,
-        },
-        orderBy: {
-          Version_created_at: "desc"
-        },
-        select: {
-          Version_v_number: true,
-          Version_file_path: true,
-          Version_approved: true,
-          Version_id: true,
-          modules: {
-            select: {
-              Module_id: true,
-              users: {
-                select: {
-                  User_discord_id: true
-                }
-              }
-            }
-          }
+    // file exists ?
+    if(filePath !== ''){
+      const fp = path.join(__dirname, basePath, filePath);
+      res.download(fp, (err) => {
+        if (err) {
+          res.status(404).send('File not found');
+        } else {
+          console.log('File download successful');
         }
       });
-      let filePath = "";
-      
-      if(v.Version_approved){
-        filePath = v.Version_file_path;
-      }
-      
-      if(filePath !== ''){
-        
-        addCount(v.modules.Module_id, v.Version_id)
-
-        const fp = path.join(__dirname, basePath, filePath);
-        res.download(fp, (err) => {
-          if (err) {
-            res.status(404).send('File not found');
-          } else {
-            console.log('File download successful');
-          }
-        });
-      }else{
-        res.status(401).json({ message: 'Version not approved' });
-      }
+    }else{
+      res.status(401).json({ message: 'No approved version found' });
     }
     
   } catch (error) {
@@ -133,7 +107,7 @@ const func = async (req, res) => {
 async function addCount(moduleID, versionID){
   await prisma.modules.update({
     where: {
-      Module_id: moduleID,
+      Module_id: parseInt(moduleID),
     },
     data: {
       Module_downloads: {
