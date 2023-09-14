1de4a8707194c79a609ef7369692569b516c850d
RainOrigami <rain+github@codefreak.net>
2023-09-14 10:19:39 +0200

    Refactoring, permissions

--- routes/Download/Module.mjs
+++ routes/Download/Module.mjs
@@ -19,92 +19,86 @@ const metadata = {
 };
 
 const func = async (req, res) => {
-  try {
-
-    let versionsQuery = {
-      where: {
-        modules: {
-          Module_name: req.params.moduleName
-        }
-      },
-      orderBy: {
-        Version_created_at: "desc"
-      },
-      select: {
-        Version_v_number: true,
-        Version_file_path: true,
-        Version_approved: true,
-        Version_id: true,
-        modules: {
-          select: {
-            Module_id: true,
-            users: {
-              select: {
-                User_discord_id: true
-              }
+  let versionsQuery = {
+    where: {
+      modules: {
+        Module_name: req.params.moduleName
+      }
+    },
+    orderBy: {
+      Version_created_at: "desc"
+    },
+    select: {
+      Version_v_number: true,
+      Version_file_path: true,
+      Version_approved: true,
+      Version_id: true,
+      modules: {
+        select: {
+          Module_id: true,
+          users: {
+            select: {
+              User_discord_id: true
             }
           }
         }
       }
+    }
+  };
+
+  if (req.user && req.user.roles !== 'ADMIN' && req.user.roles !== 'MODERATOR') {
+    // Logged in users that are not admins or moderators can only see approved or their own versions
+    versionsQuery.where = {
+      ...versionsQuery.where,
+      OR: [
+        { User_discord_id: req.user.User_discord_id },
+        { Version_approved: true }
+      ]
     };
+  } else if (!req.user) {
+    // If user is not logged in, only approved versions are shown
+    versionsQuery.where = {
+      ...versionsQuery.where,
+      Version_approved: true
+    };
+  }
 
-    // If want latest version
-    if(req.params.version != 'latest'){
-      const searchCondition = {
-        modules: {
-          Module_name: req.params.moduleName,
-        },
-        Version_v_number: req.params.version,
-      }
-
-      versionsQuery.where = {
-        ...versionsQuery.where,
-        ...searchCondition
-      };
-
-      const version = await prisma.versions.findFirst(versionsQuery)
-
-      console.log(version)
-
-      if(version && version.Version_approved){
-        filePath = version.Version_file_path;
-      }
-
-    }
+  if (req.params.version != 'latest') {
     // If you specify the version manually
-    else{
-
-      const lastVersion = await prisma.versions.findMany(versionsQuery)
-            
-      for (let i = 0; i < lastVersion.length; i++) {
-        const el  = lastVersion[i];
-        if(el.Version_approved){
-          filePath = el.Version_file_path;
-        }
-      }
+    const searchCondition = {
+      Version_v_number: req.params.version,
     }
 
-    // file exists ?
-    if(filePath !== ''){
-      const fp = path.join(__dirname, basePath, filePath);
-      res.download(fp, (err) => {
-        if (err) {
-          res.status(404).send('File not found');
-        } else {
-          console.log('File download successful');
-        }
-      });
-    }else{
-      res.status(401).json({ message: 'No approved version found' });
+    versionsQuery.where = {
+      ...versionsQuery.where,
+      ...searchCondition
+    };
+  }
+
+  try {
+    const version = await prisma.versions.findFirstOrThrow(versionsQuery)
+    if (version) {
+      filePath = version.Version_file_path;
     }
-    
   } catch (error) {
-    console.error('Error fetching reports:', error);
+    console.error('Error fetching versions:', error);
     res.status(500).json({ message: 'Internal server error.' });
+  }
+
+  // file exists ?
+  if (filePath !== '') {
+    const fp = path.join(__dirname, basePath, filePath);
+    res.download(fp, (err) => {
+      if (err) {
+        res.status(404).send('File not found');
+      }
+    });
+  } else {
+    res.status(401).json({ message: 'No version found' });
   }
 };
 
-async function addCount(moduleID, versionID){
+async function addCount(moduleID, versionID) {
   await prisma.modules.update({
     where: {
       Module_id: parseInt(moduleID),
@@ -128,4 +122,4 @@ async function addCount(moduleID, versionID){
     },
   });
 }
-export { func, metadata };
+export { func as func, metadata };
