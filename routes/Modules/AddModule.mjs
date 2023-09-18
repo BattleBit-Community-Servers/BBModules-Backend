7a598d60450a93cd7738112862b609559764a898
RainOrigami <rain+github@codefreak.net>
2023-09-18 16:57:42 +0200

    Module uploading

--- routes/Modules/AddModule.mjs
+++ routes/Modules/AddModule.mjs
@@ -1,104 +1,73 @@
 // AddModule.mjs
 
 import { promises as fsPromises } from 'fs';
-import path from 'path';
 import prisma from '../../database/Prisma.mjs';
 import checker from '../../APIChecker.mjs';
 import Utils from '../../Utils.mjs';
-import chalk from 'chalk';
+import path from 'path';
+import { rimraf } from 'rimraf'
 
-async function processFile(file) {
+async function createDirectoryRecursively(directoryPath) {
+  const directories = directoryPath.split(path.sep);
+  let currentPath = '';
 
-  if(! file.name.endsWith('.cs')){
-    return "BAD_EXTENSION";
-  }
-
-  const uploadPath = global.appRoot + '/TEMP/' + file.name;
-  try{
-    await fsPromises.writeFile(uploadPath, file.data);
-    const retData = {
-      path: uploadPath,
-      name: file.name
-    }
-    return retData;
-  } catch (err) {
-    console.error('Error saving the file:', err);
-  }
-}
-
-async function createDirectories(filePath) {
-  const dirname = path.dirname(filePath);
-  try {
-    await fsPromises.access(dirname);
-  } catch (error) {
-    await createDirectories(dirname);
-    try {
-      await fsPromises.mkdir(dirname);
-    } catch (err) {
-      if (err.code !== 'EEXIST') {
-        throw err;
-      }
-    }
-  }
-}
-
-async function moveFile(sourcePath, destinationPath) {
-  try {
-    // Ensure the destination directory and its parent directories are created
-    await createDirectories(destinationPath);
-
-    // Check if the source file exists
-    const sourceExists = await fsPromises.access(sourcePath).then(() => true).catch(() => false);
-
-    if (sourceExists) {
-      await fsPromises.rename(sourcePath, destinationPath+'.cs');
+  for (const directory of directories) {
+    if (currentPath === '') {
+      currentPath = directory;
     } else {
-      console.error('Source file does not exist:', sourcePath);
-    }
-  } catch (err) {
-    console.error('Error moving the file:', err);
-  }
-}
-
-async function checkModule(Path, Module, UserId, name, markDown){
-  const APICheck = await checker.check(Path);
-  let DestinationPath = "";
-  console.log(APICheck)
-
-  if(APICheck.Errors !== null){
-    console.log(chalk.red(APICheck.Errors));
-  }else{
-
-    if(Module){
-      DestinationPath = global.appRoot + '/MODULES/' + UserId + '/' + Module + '/' + APICheck.Version + '/' + APICheck.Name;
-    }else{
-      const createdModule = await prisma.modules.create({
-        data: {
-          Module_name: APICheck.Name,
-          Module_author_id: UserId,
-          Module_shortdesc: APICheck.Description,
-          Module_markdown: markDown,
-        },
-      });
-      DestinationPath = global.appRoot + '/MODULES/' + UserId + '/' + createdModule.Module_id + '/' + APICheck.Version + '/' + APICheck.Name;
+      currentPath = path.join(currentPath, directory);
     }
 
-    // TODO : dependencies
-
-    await moveFile(Path, DestinationPath);
+    try {
+      await fsPromises.access(currentPath);
+    } catch (error) {
+      await fsPromises.mkdir(currentPath);
+    }
   }
 }
 
 const func = async (req, res) => {
+  if (!req.files.file.name.toLowerCase().endsWith('.cs')) {
+    res.status(400).json({ message: 'Module extension is not .cs' });
+    return;
+  }
+
+  const binaryDependencies = JSON.parse(req.body.binary_dependencies);
+
+  const uploadFolder = path.join(global.appRoot, "TEMP", Math.floor(Date.now() / 1000) + "-" + Math.floor(Math.random() * 99999999));
+  const uploadPath = path.join(uploadFolder, req.files.file.name);
+
+  await createDirectoryRecursively(uploadFolder);
+
   try {
+    await fsPromises.writeFile(uploadPath, req.files.file.data);
+  } catch (err) {
+    console.error('Error saving the file:', err);
+    res.status(500).json({ message: 'Unable to save file' });
+    return;
+  }
 
-    const { path, name } = await processFile(req.files.file);
-    console.log(path, name)
-    const markDown = req.body.markDown;
+  //const { verificationOutput } = await spawnPromise(process.env.VERIFICATION_TOOL, [uploadPath]);
+  let verificationResult;
+  try {
+    verificationResult = await checker.check(uploadPath);
+  } catch (err) {
+    console.error('Error running the verification tool:', err);
+    res.status(500).json({ message: 'Unable to run verification tool' });
+    return;
+  }
 
-    const module = await prisma.modules.findUnique({
+  if (!verificationResult.Success && (!binaryDependencies || binaryDependencies.length === 0)) {
+    res.status(400).json({ message: 'Verification failed with errors:\n' + verificationResult.Errors });
+    rimraf(uploadFolder);
+    return;
+  }
+
+  let module;
+  try {
+    module = await prisma.modules.findUnique({
       where: {
-        Module_name: name.replace('.cs', ''),
+        Module_name: verificationResult.Name,
       },
       select: {
         Module_name: true,
@@ -118,31 +87,210 @@ const func = async (req, res) => {
         }
       }
     });
-
-    console.log('module: ')
-    console.log(module)
-    
-    if(module){
-      if(req.user.User_discord_id == module.users.User_discord_id){
-        if(! Utils.arrayContainsJsonValue(module.versions,  "Version_v_number", version)){
-          checkModule(path, module.Module_id, req.user.User_id, name, markDown)
-        }else res.status(409).json({"code": "resource_conflict", "message": "Your module already have this version."});
-      }else res.status(409).json({"code": "resource_conflict", "message": "A module with this name already exists."});
-    }else{
-      checkModule(path, null, req.user.User_id, name, markDown)
-    }
-
-    
-  } catch (error) {
-    console.error('Error fetching reports:', error);
-    res.status(500).json({ message: 'Internal server error.' });
+  } catch (err) {
+    console.error('Error querying the database:', err);
+    res.status(500).json({ message: 'Unable to query database' });
+    rimraf(uploadFolder);
+    return;
   }
+
+  if (module) {
+    if (req.user.User_discord_id == module.users.User_discord_id) {
+      if (Utils.arrayContainsJsonValue(module.versions, "Version_v_number", verificationResult.Version)) {
+        res.status(400).json({ "message": `The version number ${verificationResult.Name} already exists on your module ${verificationResult.Version}` });
+        rimraf(uploadFolder);
+        return;
+      }
+    } else {
+      res.status(400).json({ "message": "A module with this name already exists." });
+      rimraf(uploadFolder);
+      return;
+    }
+  }
+
+  const moduleDependencyNames = verificationResult.RequiredDependencies.concat(verificationResult.OptionalDependencies);
+  let moduleDependencies;
+  try {
+    moduleDependencies = await prisma.modules.findMany({
+      where: {
+        Module_name: {
+          in: moduleDependencyNames,
+        },
+      },
+      select: {
+        Module_id: true,
+        Module_name: true,
+      },
+    });
+  } catch (err) {
+    console.error('Error querying the database:', err);
+    res.status(500).json({ message: 'Unable to query database' });
+    rimraf(uploadFolder);
+    return;
+  }
+
+  const missingDependencies = moduleDependencyNames.filter((dependencyName) => {
+    return !Utils.arrayContainsJsonValue(moduleDependencies, 'Module_name', dependencyName);
+  });
+
+  if (missingDependencies.length > 0) {
+    res.status(400).json({ message: `Missing dependencies: ${missingDependencies.join(', ')}` });
+    rimraf(uploadFolder);
+    return;
+  }
+
+  let changelog = "Initial upload";
+  if (req.body.changelog?.length > 0) {
+    changelog = req.body.changelog;
+  } else if (module) {
+    changelog = "No changelog provided";
+  }
+
+  if (!module) {
+    try {
+      module = await prisma.modules.create({
+        data: {
+          Module_name: verificationResult.Name,
+          Module_author_id: req.user.User_id,
+          Module_shortdesc: verificationResult.Description,
+          Module_markdown: verificationResult.Description,
+        },
+      });
+    } catch (err) {
+      console.error('Error querying the database:', err);
+      res.status(500).json({ message: 'Unable to query database' });
+      rimraf(uploadFolder);
+      return;
+    }
+  }
+
+  let version;
+  try {
+    version = await prisma.versions.create({
+      data: {
+        Version_v_number: verificationResult.Version,
+        Version_module_id: module.Module_id,
+        Version_file_path: "",
+        Version_changelog: changelog,
+      }
+    });
+  } catch (err) {
+    console.error('Error querying the database:', err);
+    res.status(500).json({ message: 'Unable to query database' });
+    rimraf(uploadFolder);
+    return;
+  }
+
+  if (moduleDependencies.length > 0) {
+    try {
+      await prisma.dependencies.createMany({
+        data: moduleDependencies.map((dependency) => {
+          return {
+            Dependency_version_id: version.Version_id,
+            Dependency_module_id: dependency.Module_id,
+            Dependency_type: Utils.arrayContainsJsonValue(verificationResult.RequiredDependencies, dependency.Module_name) ? 'required' : 'optional',
+          };
+        }),
+      });
+    } catch (err) {
+      console.error('Error querying the database:', err);
+      res.status(500).json({ message: 'Unable to query database' });
+      rimraf(uploadFolder);
+
+      try {
+        await prisma.versions.delete({
+          where: {
+            Version_id: version.Version_id,
+          },
+        });
+      } catch (err) { }
+
+      return;
+    }
+  }
+
+  if (binaryDependencies && binaryDependencies.length > 0) {
+    try {
+      await prisma.dependencies.createMany({
+        data: binaryDependencies.map((binaryDependency) => {
+          return {
+            Dependency_version_id: version.Version_id,
+            Dependency_binary_text: binaryDependency.name,
+            Dependency_type: "binary"
+          };
+        }),
+      });
+    } catch (err) {
+      console.error('Error querying the database:', err);
+      res.status(500).json({ message: 'Unable to query database' });
+      rimraf(uploadFolder);
+
+      try {
+        await prisma.versions.delete({
+          where: {
+            Version_id: version.Version_id,
+          },
+        });
+      } catch (err) { }
+
+      return;
+    }
+  }
+
+  const modulePath = path.join(global.appRoot, "MODULES", module.Module_id.toString(), version.Version_v_number);
+  await createDirectoryRecursively(modulePath);
+  const moduleFilePath = path.join(modulePath, verificationResult.Name + ".cs");
+
+  try {
+    await fsPromises.rename(uploadPath, moduleFilePath);
+  } catch (err) {
+    console.error('Error moving the file:', err);
+    res.status(500).json({ message: 'Unable to move file' });
+    rimraf(uploadFolder);
+
+    try {
+      await prisma.versions.delete({
+        where: {
+          Version_id: version.Version_id,
+        },
+      });
+    } catch (err) { }
+
+    return;
+  }
+
+  try {
+    await prisma.versions.update({
+      where: {
+        Version_id: version.Version_id,
+      },
+      data: {
+        Version_file_path: moduleFilePath,
+      },
+    });
+  } catch (err) {
+    console.error('Error querying the database:', err);
+    res.status(500).json({ message: 'Unable to query database' });
+    rimraf(uploadFolder);
+
+    try {
+      await prisma.versions.delete({
+        where: {
+          Version_id: version.Version_id,
+        },
+      });
+    } catch (err) { }
+
+    return;
+  }
+
+  res.status(200).json({ Module_id: module.Module_id });
 };
 
 const metadata = {
   type: 'POST',
   url: '/AddModule',
-  auth: false,
+  auth: true,
   role: ['ADMIN', 'MODERATOR', 'USER'],
 };
 
