5afb3a062f2b91c79f7017fe192b5fbd443e661e
RainOrigami <rain+github@codefreak.net>
2023-09-21 21:38:31 +0200

    Adds guild verification to discord strategy

--- DiscordStrategy.mjs
+++ DiscordStrategy.mjs
@@ -7,10 +7,10 @@ import { Strategy as DiscordStrategy } from 'passport-discord';
 import prisma from './database/Prisma.mjs';
 
 passport.serializeUser((user, done) => {
-  done(null, { id: user.User_discord_id , roles: user.User_roles })
+  done(null, { id: user.User_discord_id, roles: user.User_roles })
 });
 
-passport.deserializeUser(async ({id,role}, done) => {
+passport.deserializeUser(async ({ id, role }, done) => {
   const user = await prisma.users.findFirst({
     where: {
       User_discord_id: id,
@@ -18,14 +18,14 @@ passport.deserializeUser(async ({id,role}, done) => {
   });
 
   if (user) done(null, user)
-  else  done(new Error("User not found"))
+  else done(new Error("User not found"))
 });
 
 passport.use(new DiscordStrategy({
   clientID: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
   callbackURL: process.env.CLIENT_REDIRECT,
-  scope: [ 'identify', 'guilds' ]
+  scope: ['identify', 'guilds']
 }, async (accessToken, refreshToken, profile, done) => {
   //console.log(profile)
   try {
@@ -38,6 +38,19 @@ passport.use(new DiscordStrategy({
     // if user banned
     if (existingUser && existingUser.User_is_banned) {
       return done("User is banned");
+    }
+
+    // if user not in guild
+    let inGuild = false;
+    profile.guilds.forEach(element => {
+      if (element.id === process.env.DISCORD_GUILD_ID) {
+        inGuild = true;
+        return;
+      }
+    });
+
+    if (!inGuild) {
+      return done("User is not in the guild. Join https://discord.gg/FTkj9xUvHh and try again.");
     }
 
     if (existingUser) {
