8a6973b8802d3df9bbe39fedf87d3e0d2ea9530c
JellisyWoes <jelly@jellisy.live>
2025-11-29 03:17:54 -0500

    Removed old backend

--- README.md
+++
@@ -1,6 +0,0 @@
-# BBModules-Backend
-A place to store modules for the BattleBit Modular API
-
-
-
-Jelly Dev Testing
--- database/Prisma.mjs
+++
@@ -1,7 +0,0 @@
-// Prisma.js
-
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
-
-export default prisma;
